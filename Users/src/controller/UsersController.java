5cc9de010377a1b8657ded99f9cbe43ad853b499
30413 <30413@DESKTOP-JQQDVD9>
2020-06-25 18:16:09 +0900

    0625 index.html 요청도 처리할 수 있게 URL패턴 변경

--- Users/src/controller/UsersController.java
+++ Users/src/controller/UsersController.java
@@ -1,38 +1,50 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UsersController
- */
-@WebServlet("/users/*")
+import service.UsersService;
+import service.UsersServiceImpl;
+
+
+@WebServlet({"/index.html","/users/*"})
 public class UsersController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private UsersService usersService;
+    
     public UsersController() {
         super();
-        // TODO Auto-generated constructor stub
+        usersService = UsersServiceImpl.sharedInstance();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = 
+			requestURI.substring(contextPath.length());
+		
+		String method = request.getMethod();
+		
+		if(command.equals("/index.html")) {
+			response.sendRedirect("./");
+		}
+		//단순 페이지 이동은 포워딩
+		else if(command.equals("/user/main")) {
+			RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("../member/main.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
