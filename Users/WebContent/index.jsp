8fa0a1a5cc194de40d9dbf52472fcf271da0e177
30413 <30413@DESKTOP-JQQDVD9>
2020-06-25 18:21:01 +0900

    0625 작업

--- Users/WebContent/index.jsp
+++ Users/WebContent/index.jsp
@@ -4,9 +4,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>유저 관리</title>
 </head>
 <body>
-
+	<!-- 바로 user/main으로 포워딩 하도록 해주는 코드 
+	전자 정보 프레임워크가 만들어주는 프로젝트에서 index.jsp에 보면
+	이런 코드가 존재합니다.-->
+	<jsp:forward page="user/main"></jsp:forward>
 </body>
 </html>
--- Users/src/service/UsersServiceImpl.java
+++ Users/src/service/UsersServiceImpl.java
@@ -1,5 +1,21 @@
 package service;
 
-public class UsersServiceImpl implements UsersService {
+import dao.UsersDao;
 
+public class UsersServiceImpl implements UsersService {
+		//Service 에서 사용할 UsersDao 변수
+		private UsersDao usersDao;
+		
+		private UsersServiceImpl() {
+			usersDao = UsersDao.sharedInstance();
+		}
+		
+		private static UsersService usersService;
+		
+		public static UsersService sharedInstance() {
+			if(usersService == null) {
+				usersService = new UsersServiceImpl();
+			}
+			return usersService;
+		}
 }
